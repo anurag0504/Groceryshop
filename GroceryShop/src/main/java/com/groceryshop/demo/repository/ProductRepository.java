2d56e932744cbcb57eb19161c11388857e021cf8
chatrapathi-repo <73647738+chatrapathi-repo@users.noreply.github.com>
2020-11-10 08:32:04 +0530

    Delete ProductRepository.java

--- GroceryShop/src/main/java/com/groceryshop/demo/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.groceryshop.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.groceryshop.demo.entitites.ProductEntity;
-
-public interface ProductRepository extends JpaRepository<ProductEntity, Integer> {
-
-}
