76fc53588bc94e940dd04558316bb7bb0649ac9a
chatrapathi-repo <73647738+chatrapathi-repo@users.noreply.github.com>
2020-11-13 20:03:26 +0530

    Add files via upload

--- GroceryShop/src/main/java/com/groceryshop/demo/entitites/ProductEntity.java
+++ GroceryShop/src/main/java/com/groceryshop/demo/entitites/ProductEntity.java
@@ -1,101 +1,106 @@
-package com.groceryshop.demo.entitites;
+package com.cg.gsm.app.entities;
 
 import java.sql.Timestamp;
 import java.util.Arrays;
-
-
 import javax.persistence.Entity;
-
 
 @Entity
 public class ProductEntity extends BaseEntity {
 
-	private String name;
-	private String categoryId;
-	private String productCode;
-	private String price;
-	private String description;
-	private String quantity;
-	private byte[] image;
-	//private Blob image;
-	
-	public ProductEntity() {
-		super();
-	}
+    private String name;
+    private String categoryId;
+    private String productCode;
+    private String price;
+    private String description;
+    private String quantity;
+    private byte[] image;
 
-	public ProductEntity( String createdBy, String modifiedBy, Timestamp createdDateTime,
-			Timestamp modifiedDateTime,String name, String code, String price, String description, String quantity, byte[] image) {
-		super(createdBy,modifiedBy,createdDateTime,modifiedDateTime);
-		this.name = name;
-		this.productCode = code;
-		this.price = price;
-		this.description = description;
-		this.quantity = quantity; 
-		this.image = image;
-	}
+    public ProductEntity() {
+        super();
+    }
 
-	public String getName() {
-		return name;
-	}
+    public ProductEntity(long id, String createdBy, String modifiedBy, Timestamp createdDateTime, Timestamp modifiedDateTime, String name, String categoryId, String productCode,
+            String price, String description, String quantity, byte[] image) {
+        super(id, createdBy, modifiedBy, createdDateTime, modifiedDateTime);
+        this.name = name;
+        this.categoryId = categoryId;
+        this.productCode = productCode;
+        this.price = price;
+        this.description = description;
+        this.quantity = quantity;
+        this.image = image;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String getProductCode() {
-		return productCode;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void setProductCode(String code) {
-		this.productCode = code;
-	}
+    public String getCategoryId() {
+        return categoryId;
+    }
 
-	public String getPrice() {
-		return price;
-	}
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
 
-	public void setPrice(String price) {
-		this.price = price;
-	}
+    public String getProductCode() {
+        return productCode;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public String getPrice() {
+        return price;
+    }
 
-	public String getQuantity() {
-		return quantity;
-	}
+    public void setPrice(String price) {
+        this.price = price;
+    }
 
-	public void setQuantity(String quantity) {
-		this.quantity = quantity;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public byte[] getImage() {
-		return image;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	public void setImage(byte[] image) {
-		this.image = image;
-	}
+    public String getQuantity() {
+        return quantity;
+    }
 
-	@Override
-	public String toString() {
-		return "ProductEntity [name=" + name + ", Product code=" + productCode + ", price=" + price + ", description=" + description
-				+ ", quantity=" + quantity + ", image=" + Arrays.toString(image) + "]";
-	}
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
 
-	@Override
-	public String getKey() {
-		return String.valueOf(id);
-	}
+    public byte[] getImage() {
+        return image;
+    }
 
-	@Override
-	public String getValue() {
-		return name ;
-	}
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductEntity [name=" + name + ", categoryId=" + categoryId + ", productCode=" + productCode + ", price=" + price + ", description=" + description + ", quantity="
+                + quantity + ", image=" + Arrays.toString(image) + "]";
+    }
+
+    @Override
+    public String getKey() {
+        return String.valueOf(id);
+    }
+
+    @Override
+    public String getValue() {
+        return name;
+    }
 
 }
