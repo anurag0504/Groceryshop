6306a0120f5871d709336f7a4050efccaa7b15ad
chatrapathi-repo <73647738+chatrapathi-repo@users.noreply.github.com>
2020-11-13 20:06:40 +0530

    Add files via upload

--- GroceryShop/src/main/java/com/groceryshop/demo/service/ProductServiceImplements.java
+++ GroceryShop/src/main/java/com/groceryshop/demo/service/ProductServiceImplements.java
@@ -1,5 +1,6 @@
 package com.cg.gsm.app.service;
 
+import java.awt.print.Pageable;
 import java.sql.Blob;
 import java.sql.SQLException;
 import java.util.List;
@@ -11,92 +12,147 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cg.gsm.app.entities.ProductEntity;
-import com.cg.gsm.app.exception.DuplicateRecordException;
-import com.cg.gsm.app.exception.RecordNotFoundException;
 import com.cg.gsm.app.repository.ProductDAOInt;
 
 @Service
-public class ProductServiceImplements implements ProductServiceInt{
-	
-	@Autowired
-	ProductDAOInt repo;
-	
-	public long add(ProductEntity bean) throws DuplicateRecordException {
-		ProductEntity product;
-		if(repo.existsById(bean.getId()))
-		{
-			throw new DuplicateRecordException("Duplicate record found");
-		}
-		else {
-			product=repo.save(bean);
-		}
-		return product.getId();
-	}
+public class ProductServiceImplements implements ProductServiceInt {
 
-	
-	public ProductEntity update(ProductEntity bean){
-		ProductEntity product;
-		if(repo.existsById(bean.getId()))
-		{
-			product=repo.save(bean);
-		}
-		else
-		{
-			throw new RecordNotFoundException("No record found with id:"+bean.getId());
-		}
-		return product;
-	}
+    @Autowired
+    ProductDAOInt repo;
 
-	
-	public boolean delete(ProductEntity bean) {
-		
-		if(repo.existsById(bean.getId()))
-		{
-			repo.deleteById(bean.getId());
-			return true;
-		}
-		else
-		{
-			throw new RecordNotFoundException("No record found with id:"+bean.getId()+"to delete");
-		}
-	}
+    /**
+     * addProduct
+     * <p>
+     * It adds a product into table and returns primary key
+     * </p>
+     * 
+     * @param bean
+     * @return long
+     */
+    public long add(ProductEntity bean) {
+        if (repo.existsById(bean.getId())) {
+            return 0;
+        } else {
+            return repo.save(bean).getId();
+        }
+    }
 
-	
-	public List<ProductEntity> findByName(String name) {
-		
-		List<ProductEntity> products;
-		products=repo.findByName(name);
-		return products;
-	}
+    /**
+     * updateProduct
+     * <p>
+     * It updates a product which is already exists
+     * </p>
+     * 
+     * @param bean
+     * @return ProductEntity
+     */
 
-	
-	public List<ProductEntity> findByProductCode(String code) {
-		
-		List<ProductEntity> products;
-		products=repo.findByProductCode(code);
-		return products;
-	}
+    public ProductEntity update(ProductEntity bean) {
+        if (repo.existsById(bean.getId())) {
+            return repo.save(bean);
+        } else {
+            return new ProductEntity();
+        }
+    }
 
-	
-	public ProductEntity findByPk(long id) {
-		return repo.findById(id).orElseThrow(()->new RecordNotFoundException("No Record found with id:"+id));
-	}
+    /**
+     * DeleteProduct
+     * <p>
+     * It updates a product which is already exists
+     * </p>
+     * 
+     * @param bean
+     * @return ProductEntity
+     */
+    public boolean delete(Long id) {
 
-	public List<ProductEntity> search(ProductEntity bean, long pageNo, int pageSize) {
-		
-		return null;
-	}
+        if (repo.existsById(id)) {
+            repo.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	
-	public List<ProductEntity> search(ProductEntity bean) {
-		List<ProductEntity> products=repo.findByName(bean.getName());
-		return products;
-	}
+    /**
+     * findByName
+     * <p>
+     * Retrieving the products with name as a parameter
+     * </p>
+     * 
+     * @param bean
+     * @return List<ProductEntity>
+     */
 
-	
-	public Blob getImageById(long id) throws SerialException, SQLException {
-		Blob blob=new SerialBlob(repo.findById(id).get().getImage());
-		return blob;
-	}
+    public List<ProductEntity> findByName(String name) {
+
+        List<ProductEntity> products;
+        products = repo.findByName(name);
+        return products;
+    }
+
+    /**
+     * findByProductCode
+     * <p>
+     * Retrieving the products with productCode as a parameter
+     * </p>
+     * 
+     * @param bean
+     * @return List<ProductEntity>
+     */
+    public List<ProductEntity> findByProductCode(String code) {
+
+        List<ProductEntity> products;
+        products = repo.findByProductCode(code);
+        return products;
+    }
+
+    /**
+     * findByPk
+     * <p>
+     * finding a product with its primary key
+     * </p>
+     * 
+     * @param bean
+     * @return ProductEntity
+     */
+    public ProductEntity findByPk(long id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    /**
+     * search
+     * <p>
+     * searching for products with product as a parameter
+     * </p>
+     * 
+     * @param bean
+     * @return List<ProductEntity>
+     */
+    public List<ProductEntity> search(ProductEntity bean) {
+        List<ProductEntity> products = repo.findByName(bean.getName());
+        return products;
+    }
+
+    /**
+     * getImageById
+     * <p>
+     * Retrieving an image associated with primary key
+     * </p>
+     * 
+     * @param id
+     * @return Blob(Binary large object)
+     * @throws SerialException
+     * @throws SQLException
+     */
+    public Blob getImageById(long id) throws SerialException, SQLException {
+        Blob blob = new SerialBlob(repo.findById(id).get().getImage());
+        return blob;
+    }
+
+    @Override
+    public List<ProductEntity> search(ProductEntity bean, Pageable page) {
+        return null;
+    }
 
 }
--- GroceryShop/src/main/java/com/groceryshop/demo/service/ProductServiceInt.java
+++ GroceryShop/src/main/java/com/groceryshop/demo/service/ProductServiceInt.java
@@ -1,31 +1,33 @@
-package com.groceryshop.demo.service;
+package com.cg.gsm.app.service;
 
+import java.awt.print.Pageable;
 import java.sql.Blob;
 import java.sql.SQLException;
 import java.util.List;
 
 import javax.sql.rowset.serial.SerialException;
 
-import com.groceryshop.demo.entitites.ProductEntity;
-import com.groceryshop.demo.exception.DuplicateRecordException;
+import com.cg.gsm.app.entities.ProductEntity;
+import com.cg.gsm.app.exception.DuplicateRecordException;
 
 public interface ProductServiceInt {
 
-public long add(ProductEntity bean) throws DuplicateRecordException;
-	
-	public void update(ProductEntity bean) throws DuplicateRecordException;
-	
-	public void delete(ProductEntity bean);
-	
-	public ProductEntity findByName(String name);
-	
-	public ProductEntity findByCode(String code);
-	
-	public ProductEntity findByPk(long id);
-	
-	public List<ProductEntity> search(ProductEntity bean, long pageNo, int pageSize);
-	
-	public List<ProductEntity> search(ProductEntity bean);
-	
-	public Blob getImageById(long id) throws SerialException, SQLException;
+    public long add(ProductEntity bean) throws DuplicateRecordException;
+
+    public ProductEntity update(ProductEntity bean) throws DuplicateRecordException;
+
+    public boolean delete(Long id);
+
+    public List<ProductEntity> findByName(String name);
+
+    public List<ProductEntity> findByProductCode(String code);
+
+    public ProductEntity findByPk(long id);
+
+    public List<ProductEntity> search(ProductEntity bean, Pageable page);
+
+    public List<ProductEntity> search(ProductEntity bean);
+
+    public Blob getImageById(long id) throws SerialException, SQLException;
+
 }
